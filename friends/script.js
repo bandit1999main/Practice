0b8056cfe2874c8209e25089f65702c314bede14
BanditB <bandit1999main@gmail.com>
2025-11-15 19:35:25 +0700

    friend project
    just click and show pic of each person

---
+++ friends/script.js
@@ -0,0 +1,76 @@
+function changToGangster(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="group.jpg";
+    name.innerHTML ="Gangster"
+}
+
+function changToBird(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="bird.jpg";
+    name.innerHTML ="Bird"
+}
+
+function changToArm(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="arm.jpg";
+    name.innerHTML ="Arm"
+}
+
+function changToU(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="u.jpg";
+    name.innerHTML ="U"
+}
+
+function changToKik(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="kik.jpg";
+    name.innerHTML ="Kik"
+}
+
+function changToKwan(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="kwan.jpg";
+    name.innerHTML ="Kwan"
+}
+
+function changToNamo(){
+    /* ค้นหา แท็กที่มี id mainImage ใน html */
+    var image = document.getElementById("mainImage");
+    /* ค้นหา แท็กที่มี id friendName ใน html */
+    var name = document.getElementById("friendName");
+
+    //เปลี่ยน คำของมัน
+    image.src ="namo.jpg";
+    name.innerHTML ="Namo"
+}
